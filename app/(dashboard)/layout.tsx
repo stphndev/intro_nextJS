9fb1b77226a79eeda8c0d1c2297393e5ab228ee2
stphndev <133531810+stphndev@users.noreply.github.com>
2023-10-03 07:37:49 +0000

    Layout and Styling

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+const DashBoardLayout = ( {children} ) => {
+    return <div>
+        <h1>Dashboard</h1>
+        <div>{children}</div>
+    </div>
+}
+
+export default DashBoardLayout
--- app/docs/[id]/[title]/page.tsx
+++
@@ -1,5 +0,0 @@
-const DocsIdPage = ({ params }) => {
-    return <div>Docs Id Page {params.id} {params.title}</div>
-}
-
-export default DocsIdPage
